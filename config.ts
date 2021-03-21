1a23c030641d6bf50a40702de62668743f4ab0b0
Eduardo da Silva Kegler <ekegler@tech6.com.br>
2021-03-21 19:40:59 -0300

    Ping Vercel

--- config.ts
+++ config.ts
@@ -4,5 +4,4 @@ import * as pg from "pg";
 dotenv.config();
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 const pool = new pg.Pool({ connectionString })
-
 export { pool };
