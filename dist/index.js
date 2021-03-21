c6b10a30cf125c11c280ad5b4a7e9908bc9286eb
Eduardo da Silva Kegler <ekegler@tech6.com.br>
2021-03-21 15:47:37 -0300

    Added tables and typescript

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { pool } = require('./config');
+const app = express();
+const port = 8000;
+app.get('/api', (req, res) => {
+    return pool.query('SELECT * FROM matches', (error, results) => {
+        if (error) {
+            return res.send(error);
+        }
+        return res.send(results.rows);
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listing at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
--- init.sql
+++ init.sql
@@ -1,6 +1,30 @@
-CREATE TABLE matches (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO books (name) VALUES ('Harry Potter Team');
+CREATE TABLE sections (
+  id SERIAL PRIMARY KEY,
+  user_id int NOT NULL,
+  expire_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE teams (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  owner_id INT NOT NULL
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  home_team VARCHAR(255) NOT NULL,
+  visitor_team VARCHAR(255) NOT NULL,
+  created_by INT NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES ('harry', 'varinha');
+INSERT INTO sections (user_id, expire_date) VALUES (1, '2022-03-21 15:37:20');
+INSERT INTO teams (name, owner_id) VALUES ('HP Team 1', 1);
+INSERT INTO teams (name, owner_id) VALUES ('HP Team 2', 1);
+INSERT INTO matches (home_team, visitor_team, created_by) VALUES (1, 2, 1);
