1aa899e9b7ec23c3cc3aefd370f7e602c46c20a3
Eduardo da Silva Kegler <ekegler@tech6.com.br>
2021-03-21 18:30:22 -0300

    Remove dist, wrong files and fix dependencies versioons

--- dist/src/team/team.service.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTeamsService = void 0;
-const config_1 = require("../../config");
-exports.getTeamsService = config_1.pool.query('SELECT * FROM teams');
